c1ef8a28c69a1eb00e0ae27918f253f166a0c326
himanshusssingh <himanshusingh39397@gmail.com>
2025-07-01 17:30:47 +0530

    Adding Sample code

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from "./Hero"
+import Brokerage from "./Brokerage"
+
+function PricingPage() {
+    return ( 
+        <>
+        <Hero/>
+        <Brokerage></Brokerage>
+        </>
+     );
+}
+
+export default PricingPage;
