81893ef7f56ac578f9dd8c2714dc87578c4659cd
himanshusssingh <himanshusingh39397@gmail.com>
2025-07-01 22:33:04 +0530

    Routes Setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import HomaPage from './landing_page/home/HomePage';
-import Navbar from './landing_page/Navbar';
-import Footer from './landing_page/Footer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Navbar from "./landing_page/Navbar";
+import HomaPage from "./landing_page/home/HomePage";
+import Signup from "./landing_page/signup/Signup";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
+import Footer from "./landing_page/Footer";
+import NotFound from "./landing_page/NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <div>
-      <Navbar/>
-      <HomaPage></HomaPage>
-      <Footer/>
-    </div>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomaPage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 );
-
-
